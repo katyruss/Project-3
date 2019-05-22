fca254e07adfb029804130f77b97a0a8afd4dc2a
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-05-22 17:55:19 -0400

    init commit

---
+++ client/src/Card/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../Card/Card.css";
+
+
+const Card = props => {
+    console.log(props.name)
+    return (
+        <div className="card-height">
+            <hr />
+            {/* {
+                localStorage.hasOwnProperty('isLoggedIn') ? <button><i className="fas fa-heart"></i></button> : null
+            } */}
+            <button type = "submit"><i className="fas fa-heart heart-icon"></i></button>
+            <div className="card-result">Name: {props.name}</div>
+
+            <div className="card-result">Address: {props.vicinity}</div>
+
+            <div className="card-result">Rating: {props.rating}</div>
+
+            <div className="card-result">Open Now: {(props.open_now) ? 'Open Now' : 'Not Open Now'}</div>
+        </div>
+    )
+}
+export default Card;
+
